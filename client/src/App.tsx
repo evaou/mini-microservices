c8ba644a65c5e5d544b527935fef448d4ea569d2
evaou <speeder528@gmail.com>
2021-02-28 20:45:29 +0800

    add submit logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+/* eslint-disable import/no-anonymous-default-export */
 import PostCreate from "./PostCreate";
 
 export default () => {
   return (
-    <div className = "container">
+    <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
     </div>
