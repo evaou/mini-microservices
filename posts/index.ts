53b55573e175e45236f3d4fa5460e41f40138f23
evaou <speeder528@gmail.com>
2021-02-28 13:00:41 +0800

    add posts tsconfig and index file

---
+++ posts/index.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { randomBytes } from "crypto";
+
+const app = express();
+const posts: { [key: string]: {} } = {};
+
+app.set("case sensitive routing", true);
+app.use(bodyParser.json());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = { id, title };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
