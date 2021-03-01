31af65f5ccc14d351a30ef0745d7629f88f33e4e
evaou <speeder528@gmail.com>
2021-03-01 19:32:18 +0800

    add query api

--- query/index.ts
+++ query/index.ts
@@ -6,10 +6,39 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const posts: { [key: string]: {} } = {};
 
-app.post("/posts", (req, res) => {});
+app.get("/events", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    interface PostItem {
+      id: string;
+      title: string;
+      comments: {}[];
+    }
+
+    const post = posts[postId] as PostItem;
+
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
-  console.log("Listending on 4002");
+  console.log("Query servie is listening on 4002");
 });
