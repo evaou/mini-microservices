79913ba715b3576a052acf3241b6f48c47ac61eb
evaou <speeder528@gmail.com>
2021-03-01 11:15:18 +0800

    send event for postCreated

--- posts/index.ts
+++ posts/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { randomBytes } from "crypto";
 import * as cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,10 +14,17 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
+
   posts[id] = { id, title };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
+
   res.status(201).send(posts[id]);
 });
 
