791d8d28d44df9612a727e3ddd0e03a458dfa2e4
evaou <speeder528@gmail.com>
2021-03-05 10:09:10 +0800

    update service url for ingress

--- client/src/PostCreate.tsx
+++ client/src/PostCreate.tsx
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event: FormEvent) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", { title });
+    await axios.post("http://posts.com:4000/posts", { title });
 
     setTitle("");
   };
